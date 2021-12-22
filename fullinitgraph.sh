ac28c5c3fb8da54e56b4f36ffb22663504996fcc
elitexpro <cryptooxelite@gmail.com>
2021-12-23 06:09:41 +0900

    no message

--- fullinitgraph.sh
+++ fullinitgraph.sh
@@ -11,4 +11,6 @@ sed -i.bak -E 's#^(timeout_commit[[:space:]]+=[[:space:]]+).*$#\1"5s"#' ~/.crono
 cronosd keys add chronoswap
 cronosd start
 
+su postgres -c 'dropdb graphnodedb'
+su postgres -c 'createdb graphnodedb'
 graph-node --postgres-url postgresql://postgres:daniel@localhost:5432/graphnodedb --ethereum-rpc mainnet:http://127.0.0.1:8545 --ipfs 127.0.0.1:5001 --http-port 80 
