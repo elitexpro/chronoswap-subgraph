86edb64e87e7884feb050fbf9b9fec7d42e7ab26
elitexpro <cryptooxelite@gmail.com>
2021-12-28 10:34:58 +0000

    gg

--- scripts/cronosd.sh
+++ scripts/cronosd.sh
@@ -10,5 +10,6 @@ sed -i.bak -E 's#^(minimum-gas-prices[[:space:]]+=[[:space:]]+).*$#\1"5000000000
 sed -i.bak -E 's#^(seeds[[:space:]]+=[[:space:]]+).*$#\1"0d5cf1394a1cfde28dc8f023567222abc0f47534@cronos-seed-0.crypto.org:26656,3032073adc06d710dd512240281637c1bd0c8a7b@cronos-seed-1.crypto.org:26656,04f43116b4c6c70054d9c2b7485383df5b1ed1da@cronos-seed-2.crypto.org:26656"#' ~/.cronos/config/config.toml
 sed -i.bak -E 's#^(create_empty_blocks_interval[[:space:]]+=[[:space:]]+).*$#\1"5s"#' ~/.cronos/config/config.toml
 sed -i.bak -E 's#^(timeout_commit[[:space:]]+=[[:space:]]+).*$#\1"5s"#' ~/.cronos/config/config.toml
-cronosd keys add chronoswap
+#cronosd keys add chronoswap
 systemctl start cronosd.service
+#cronosd start
