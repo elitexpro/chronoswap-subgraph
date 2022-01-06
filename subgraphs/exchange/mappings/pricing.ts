5c731e537cda485783b399df7d3349bc247df097
elitexpro <cryptooxelite@gmail.com>
2022-01-06 11:38:55 +0900

    no message

--- subgraphs/exchange/mappings/pricing.ts
+++ subgraphs/exchange/mappings/pricing.ts
@@ -76,8 +76,8 @@ let MINIMUM_LIQUIDITY_THRESHOLD_BNB = BigDecimal.fromString("10");
  **/
 export function findBnbPerToken(token: Token): BigDecimal {
   log.info('############### findBnbPerToken param :{} == {} \n', 
-  [token.id.toString(), CNO_ADDRESS]);
-  if (token.id.toString().matchAll(CNO_ADDRESS)) {
+  [token.id, CNO_ADDRESS]);
+  if (token.id === CNO_ADDRESS.toString()) {
     log.info('++++++++++++++++++++ CNO Token Price is 1 +++++++++++++++++++++++ {}\n\n', ['']);
     return ONE_BD;
   }
